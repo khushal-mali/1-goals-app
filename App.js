7ee44314755ccc705558cafa2761b042aaf8f9d7
Khushal Mali <khushalmali8448@gmail.com>
2025-08-06 23:42:07 +0530

    update

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.khushal}>Khushal mali here! 👋</Text>
+      <Text
+        style={{
+          color: "#ef5592",
+          fontSize: 30,
+          fontWeight: 700,
+          textDecorationLine: "underline",
+          paddingHorizontal: 16,
+          borderColor: "#ef5592",
+          margin: 10,
+          borderWidth: 1,
+        }}
+      >
+        Zendria here! 💖
+      </Text>
+
+      <Button title="Klick me." color={"#212121"} />
     </View>
   );
 }
@@ -13,8 +27,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  khushal: {
+    color: "#212121",
+    fontSize: 30,
+    fontWeight: 700,
+    textDecorationLine: "underline",
   },
 });
